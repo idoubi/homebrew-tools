9e1216a7def2061f9553748ab745e307f2c2f2ad
idoubi <mikemintang@126.com>
2020-01-10 21:32:53 +0800

    v0.3.1

--- tellme.rb
+++ tellme.rb
@@ -1,12 +1,13 @@
 class Tellme < Formula
   desc "a cli tool to get information."
   homepage "https://github.com/idoubi/tellme"
-  url "https://github.com/idoubi/tellme/archive/v0.2.2.tar.gz"
-  sha256 "5345074a913d9de6609e984ff60b23b8d6a7b1f09b1e799fa49518f46b7c68ca"
+  url "https://github.com/idoubi/tellme/archive/v0.3.1.tar.gz"
+  sha256 "c0c9271b04a7e2c4a6789ea147e0470f879ca4dea2b39ab1ef3bf57d89001944"
 
   depends_on "go" => :build
 
   def install
+    system "cp", "festival.json", bin/"festival.json"
     system "go", "build", "-o", bin/"tellme"
   end
 
