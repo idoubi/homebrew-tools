c9a873703e2e3778ffe9a64e94b896e524e73093
idoubi <mikemintang@126.com>
2020-01-10 15:06:29 +0800

    v0.2.0

--- tellme.rb
+++ tellme.rb
@@ -1,8 +1,8 @@
 class Tellme < Formula
   desc "a cli tool to get information."
   homepage "https://github.com/idoubi/tellme"
-  url "https://github.com/idoubi/tellme/archive/v0.2.1.tar.gz"
-  sha256 "7676e11022b18724707cd36d24f25874922df1c587466cf6db35474ceb2c77e3"
+  url "https://github.com/idoubi/tellme/archive/v0.2.2.tar.gz"
+  sha256 "5345074a913d9de6609e984ff60b23b8d6a7b1f09b1e799fa49518f46b7c68ca"
 
   depends_on "go" => :build
 
